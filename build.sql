b82065afbfae1e947c7b2960e161a9706713c998
Alejandro Guirao <lekumberri@gmail.com>
2018-03-27 09:51:25 +0200

    Use schema import

--- build.sql
+++ build.sql
@@ -1,18 +1,19 @@
-drop table if exists master_plan;
+create schema if not exists import;
 
-create table master_plan(
-	start_time_utc text,
-	duration text,
-	date text,
-	team text,
-	spass_type text,
-	target text,
-	request_name text,
-	library_definition text,
-	title text,
-	description text
+drop table if exists import.master_plan;
+create table import.master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
 );
 
 COPY master_plan
-FROM '/home/aguirao/Desarrollo/a-curious-moon/code_and_data/data/master_plan.csv'
+FROM '/home/aguirao/Desarrollo/a-curious-moon-exercises/code_and_data/data/master_plan.csv'
 WITH DELIMITER ',' HEADER CSV;
