c4bd3efe9beda465c3fc8235635350161248ab26
Alejandro Guirao <lekumberri@gmail.com>
2018-04-12 08:53:02 +0200

    Add inner joins

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -1,3 +1,54 @@
+-- TEAM
+drop table if exists teams cascade;
+select distinct(team)
+as description
+into teams
+from import.master_plan;
+
+alter table teams
+add id serial primary key;
+
+-- SPASS TYPES
+drop table if exists spass_types cascade;
+select distinct(spass_type)
+as description
+into spass_types
+from import.master_plan;
+
+alter table spass_types
+add id serial primary key;
+
+-- TARGET
+drop table if exists targets cascade;
+select distinct(target)
+as description
+into targets
+from import.master_plan;
+
+
+alter table targets
+add id serial primary key;
+
+-- EVENT TYPES
+drop table if exists event_types cascade;
+select distinct(library_definition)
+as description
+into event_types
+from import.master_plan;
+
+alter table event_types
+add id serial primary key;
+
+--REQUESTS
+drop table if exists requests cascade;
+select distinct(request_name)
+as description
+into requests
+from import.master_plan;
+
+alter table requests
+add id serial primary key;
+
 drop table if exists events;
 
 create table events(
@@ -5,20 +56,40 @@ create table events(
 	time_stamp timestamptz not null,
 	title varchar(500),
 	description text,
-	event_type_id int,
-	spass_type_id int,
-	target_id int,
-	team_id int,
-	request_id int
+	event_type_id int references event_types(id),
+	spass_type_id int references spass_types(id),
+	target_id int references targets(id),
+	team_id int references teams(id),
+	request_id int references requests(id)
 );
 
 insert into events(
 	time_stamp,
 	title,
-	description
+	description,
+	event_type_id,
+	target_id,
+	team_id,
+	request_id,
+	spass_type_id
 )
 select
 	import.master_plan.start_time_utc::timestamptz at time zone 'UTC',
 	import.master_plan.title,
-	import.master_plan.description
-from import.master_plan;
+	import.master_plan.description,
+	event_types.id as event_type_id,
+	targets.id as target_id,
+	teams.id as team_id,
+	requests.id as request_id,
+	spass_types.id as spass_type_id
+from import.master_plan
+inner join event_types
+  on event_types.description = import.master_plan.library_definition
+inner join targets
+  on targets.description = import.master_plan.target
+inner join teams
+  on teams.description = import.master_plan.team
+inner join requests
+  on requests.description = import.master_plan.request_name
+inner join spass_types
+  on spass_types.description = import.master_plan.spass_type;
