c9af6357a15fafccd33a0b8d4965bc80f9dc05bb
Alejandro Guirao <lekumberri@gmail.com>
2018-04-19 09:19:04 +0200

    Add creation of import.master_plan

--- scripts/import.sql
+++ scripts/import.sql
@@ -5,3 +5,18 @@ create table mission_plan(
 	  title varchar(100),
 	  description text
 );
+
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
