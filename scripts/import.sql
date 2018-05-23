4416c3c6ae71f835df1bed9ff10a9b126d66f0ef
Alejandro Guirao <lekumberri@gmail.com>
2018-05-23 10:50:15 +0200

    Fix import script

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,22 +1,22 @@
 drop table if exists mission_plan;
 create table mission_plan(
-	  id serial primary key,
-	  the_date date,
-	  title varchar(100),
-	  description text
+	id serial primary key,
+	the_date date,
+	title varchar(100),
+	description text
 );
 
 create schema if not exists import;
 drop table if exists import.master_plan;
 create table import.master_plan(
-  start_time_utc text,
-  duration text,
-  date text,
-  team text,
-  spass_type text,
-  target text,
-  request_name text,
-  library_definition text,
-  title text,
-  description text
+	start_time_utc text,
+	duration text,
+	date text,
+	team text,
+	spass_type text,
+	target text,
+	request_name text,
+	library_definition text,
+	title text,
+	description text
 );
